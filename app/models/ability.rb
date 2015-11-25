d4af119215657bb04bdb2507748b3bff911c1357
Daniel Mayle <dmayle012@gmail.com>
2015-11-24 17:42:42 -0800

    Error fixed?

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user not logged in)
     can :manage, User, id: user.id
-end
+  end
 
 
     # Define abilities for the passed in user here. For example:
