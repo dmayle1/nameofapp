5ed583a9f65290e0ae1dd8e43b8fa381ad942f4b
Daniel Mayle <dmayle012@gmail.com>
2015-12-04 13:09:01 -0800

    Validation and Paginiation added

--- Gemfile
+++ Gemfile
@@ -54,6 +54,8 @@ end
 gem 'devise'
 
 gem 'cancancan'#, '~> 1.10'
+## Gemfile use for pagination
+gem 'will_paginate', '~> 3.0.6'
 
 ruby '2.2.1'
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ActiveRecord::Base
 has_many :orders
+validates :name, presence: true
+validates :body, presence: true
+validates :user, presence: true
+validates  :product, presence: true
+validates :rating, numericality: { only_integer: true}
 def average_rating
 	  comments.average(:rating).to_f
 	end
